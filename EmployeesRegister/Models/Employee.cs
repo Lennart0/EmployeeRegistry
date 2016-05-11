38871581a3651cd2ba55ac5ba720823c6f3e6ab5
Lennart0 <lennart0@hotmail.se>
2016-05-11 15:50:15 +0200

    First

---
+++ EmployeesRegister/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeesRegister.Models
+{
+    enum Department
+    {
+        IT,
+        HR,
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Company { get; set; }
+    }
+}
